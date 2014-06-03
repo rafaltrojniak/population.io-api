1f821fb911791375389ae8368e07709267ae90f3
Henrik Heimbuerger <henrik@heimbuerger.de>
2014-06-03 13:43:55 +0200

    Add support for JSONP responses (when header Accept:application/javascript is set).

--- population_io/settings.py
+++ population_io/settings.py
@@ -94,4 +94,9 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONPRenderer',
+    ),
 }
